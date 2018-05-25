253cd8fc5624d4bf14d9f5c31a4bf258f3bf6640
Earnest Scott <earnestdscott@gmail.com>
2018-05-24 20:23:13 -0700

    Add files via upload

--- PA3_starter/BitOutputStream.cpp
+++ PA3_starter/BitOutputStream.cpp
@@ -17,13 +17,25 @@ void BitOutputStream::flush()
 /*Use bitwise OR operation to update buff with the next bit*/
 void BitOutputStream::writeBit(int bit)
 {
-	if(nbits == 7)
+	char bit_rep = 0;
+	//cout<<"Second bit:"<<bit<<endl;
+	if(nbits == 8)
 	{
 		//I know we're calling flush on our BitOutputStream object, but im not sure im calling it right.
 		this->flush();
 	}
 	//from discussion slides
-	buf = buf | (bit <<(7 - nbits));
+	buf = (buf | (bit <<(7 - nbits)));
+	bit_rep = (bit_rep | (bit<<(7-nbits)));
+	/*idk why but this next line makes it work, for some reason it doesnt 
+	  seem like buf updates correctly*/
+
+	buf = bit_rep;
+	//tests
+	//cout<<"Third bit: "<<(bit <<(7 - nbits))<<endl;
+	//cout<< "Char_Rep: "<<buf<< endl;
+
+
 
 	/*update nbits so that next time writeBit is called, 
 	the correct next bit is written to buff*/
@@ -44,15 +56,17 @@ int main(int argc, char** argv)
 	ofstream writing;
 	//int i;
 	writing.open(argv[1]);
+	//create ostream to give as argument to initialize BitOutputStream
+	ostream & output = writing;
 
 
-	BitOutputStream * kstream = new BitOutputStream(writing);
+	BitOutputStream * kstream = new BitOutputStream(output);
 	//string test = "01001010";
-	unsigned int i = 74;
+	unsigned int i = 01001010;
 	//for(i = 0; i < test.length(); i++){
 
 		kstream->writeByte(i);
-	//kstream->flush();
+	kstream->flush();
 	writing.close();
 
 	//}
